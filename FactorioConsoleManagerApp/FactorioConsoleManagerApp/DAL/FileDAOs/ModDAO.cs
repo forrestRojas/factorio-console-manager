81b85ce8e308ce018cef80c6d6e3732ca5de919e
forrest rojas <forrest.rojas@gmail.com>
2019-03-09 10:49:44 -0500

    Renamed DAO files and implemented interfaces

---
+++ FactorioConsoleManagerApp/FactorioConsoleManagerApp/DAL/FileDAOs/ModDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FactorioConsoleManagerApp.Models;
+
+namespace FactorioConsoleManagerApp.DAL
+{
+    public class ModDAO : IModDAO
+    {
+        private readonly string filepath;
+
+        public ModDAO(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        public Mod GetModData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveModData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FactorioConsoleManagerApp/FactorioConsoleManagerApp/DAL/FileDAOs/ModListDAO.cs
@@ -0,0 +1,32 @@
+﻿using FactorioConsoleManagerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactorioConsoleManagerApp.DAL
+{
+    public class ModListDAO :IModListDAO
+    {
+        private readonly string filepath;
+
+        private class Root
+        {
+            public List<ModListItem> Mods { get; set; }
+        }
+
+        public ModListDAO(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        public IDictionary<string, ModList> GetAllModLists()
+        {
+            SortedDictionary<string, ModList>
+        }
+
+        public void SaveAllModLists()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
