0e00a8c596a4c0c03015d8d5428140adf703c676
forrest rojas <forrest.rojas@gmail.com>
2019-03-18 10:22:44 -0400

    refactorid header code

--- FactorioConsoleManagerApp/FactorioConsoleManagerApp/ConsoleLayout/Headers/Header.cs
+++ FactorioConsoleManagerApp/FactorioConsoleManagerApp/ConsoleLayout/Headers/Header.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Text;
 using FactorioConsoleManagerApp.ConsoleLayout;
+using FactorioConsoleManagerApp.ConsoleLayout.Models;
 
 namespace FactorioConsoleManagerApp.ConsoleLayout.Headers
 {
@@ -18,31 +20,58 @@ namespace FactorioConsoleManagerApp.ConsoleLayout.Headers
 
         public BoxBuilder BoxBuilder { get; private set; }
 
-        public string Color;
+        public Box Box { get; private set; }
+
+        public ConsoleColor ForgroundColor { get; set; }
+
+        public ConsoleColor BackgroundColor { get; set; }
 
 
-        public static void Title(string message, int boxWidth, int boxHeight, string boxType, string color)
+        public Header()
+        {
+            this.Box = new Box();
+            this.BoxBuilder = new BoxBuilder();
+            this.ForgroundColor = this.Box.ForgroundColor;
+            this.BackgroundColor = this.Box.BackgroundColor;
+        }
+
+
+        public void Title(Box box)
+        {
+            this.Title(box.Message, box.BoxWidth, box.BoxHeight, box.BoxType);
+        }
+
+        public void Title(string message, int boxWidth, int boxHeight, string boxType)
         {
             const int sizeAdjust = 1;
-            string bgcolor = color;
 
+            int windowWidth = Console.WindowWidth;
+            int bufferWidth = Console.BufferWidth;
+            int titleWidth = boxWidth + sizeAdjust;
+
+
+            //ConsoleColor bgcolor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color)
+
+            // Set window size if dimentions arent correct
+            if ((windowWidth != titleWidth) || (bufferWidth != titleWidth))
+            {
+                Console.SetWindowSize(titleWidth, Console.WindowHeight);
+                Console.SetBufferSize(titleWidth, Console.BufferHeight);
+            }
 
             Console.Clear();
-            // Set window size
-            Console.SetWindowSize(boxWidth + sizeAdjust, Console.WindowHeight);
-            Console.SetBufferSize(boxWidth + sizeAdjust, Console.BufferHeight);
             // Set Color
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            
-            // Box Builder
-            BoxBuilder boxBuilder = new BoxBuilder();
-            string title = boxBuilder.Build(message, boxWidth, boxHeight, boxType);
+            Console.ForegroundColor = this.ForgroundColor;
+            Console.BackgroundColor = this.BackgroundColor;
+
+            // Box Builder;
+            string title = this.BoxBuilder.Build(message, boxWidth, boxHeight, boxType);
 
             // Write to console
             Console.WriteLine(title);
             Console.ResetColor();
         }
+
 
         public static string PadSides(string str, int totalWidth, char paddingChar = ' ')
         {
