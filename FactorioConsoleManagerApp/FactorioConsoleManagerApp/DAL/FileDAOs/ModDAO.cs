f4868a57bcafa951fccdf2a7f3878b978d9af7cc
forrest rojas <forrest.rojas@gmail.com>
2019-03-22 10:11:31 -0400

    added documention

--- FactorioConsoleManagerApp/FactorioConsoleManagerApp/DAL/FileDAOs/ModDAO.cs
+++ FactorioConsoleManagerApp/FactorioConsoleManagerApp/DAL/FileDAOs/ModDAO.cs
@@ -5,20 +5,37 @@ using FactorioConsoleManagerApp.Models;
 
 namespace FactorioConsoleManagerApp.DAL
 {
+    /// <summary>
+    /// Represents a Factorio ModDAO.
+    /// </summary>
     public class ModDAO : IModDAO
     {
+        /// <summary>
+        /// The Mod's file path.
+        /// </summary>
         private readonly string filepath;
 
+        /// <summary>
+        /// Creates a ModDAO.
+        /// </summary>
+        /// <param name="filepath">The Mod's file path.</param>
         public ModDAO(string filepath)
         {
             this.filepath = filepath;
         }
 
+        /// <summary>
+        /// Gets the Mod Data.
+        /// </summary>
+        /// <returns>A <see cref="Mod"/> object.</returns>
         public Mod GetModData()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Saves the Mod data.
+        /// </summary>
         public void SaveModData()
         {
             throw new NotImplementedException();
