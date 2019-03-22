dd2d8fb533b514b7ea1c4481ba22b7dbede2ec5b
forrest rojas <forrest.rojas@gmail.com>
2019-03-22 10:06:54 -0400

    added documention

--- FactorioConsoleManagerApp/FactorioConsoleManagerApp/DAL/Interfaces/IModDAO.cs
+++ FactorioConsoleManagerApp/FactorioConsoleManagerApp/DAL/Interfaces/IModDAO.cs
@@ -5,10 +5,20 @@ using System.Text;
 
 namespace FactorioConsoleManagerApp.DAL
 {
+    /// <summary>
+    /// Represents an IModDAO interface.
+    /// </summary>
     public interface IModDAO
     {
+        /// <summary>
+        /// Gets the Mod pakage data files.
+        /// </summary>
+        /// <returns></returns>
         Mod GetModData();
 
+        /// <summary>
+        /// saves the Mod data files.
+        /// </summary>
         void SaveModData();
     }
 }
