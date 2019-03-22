13b83ce146a7d8a4b454e5fc49092ee8309bf5d9
forrest rojas <forrest.rojas@gmail.com>
2019-03-22 10:02:50 -0400

    added documention

--- FactorioConsoleManagerApp/FactorioConsoleManagerApp/CLI/ModListCLI.cs
+++ FactorioConsoleManagerApp/FactorioConsoleManagerApp/CLI/ModListCLI.cs
@@ -6,8 +6,14 @@ using System.Text;
 
 namespace FactorioConsoleManagerApp.CLI
 {
+    /// <summary>
+    /// Represents the ModListCLI
+    /// </summary>
     public class ModListCLI : MasterCLI
     {
+        /// <summary>
+        /// Runs the ModListCLI
+        /// </summary>
         public override void Run()
         {
             IModListDAO modListDAO = new ModListJsonDAO("C:/Users/Forrest Rojas/AppData/Roaming/Factorio/mods/mod-list.json", "C:/Users/Forrest Rojas/AppData/Roaming/Factorio/mods/mod-list.json");
